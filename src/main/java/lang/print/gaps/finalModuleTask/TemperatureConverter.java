6cff7ae940a9d341d6f3b98c393e66109f7fa59b
Maria Maksimova <drogzed@gmail.com>
2025-10-16 16:44:10 +0200

    fix

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        float far = ((float) temperatureCelsius * ((float) 9 /5) + 32);
+        System.out.println(far);
     }
 }
